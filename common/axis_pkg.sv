54e57fc42af3329905cf21157fddee2378d0c936
arnfol <arnfol@mail.ru>
2018-05-26 16:40:30 +0300

    src. create folder with common sources

---
+++ common/axis_pkg.sv
@@ -0,0 +1,47 @@
+/*
+    ------------------------------------------------------------------------------
+    -- The MIT License (MIT)
+    --
+    -- Copyright (c) <2018> Konovalov Vitaliy
+    --
+    -- Permission is hereby granted, free of charge, to any person obtaining a copy
+    -- of this software and associated documentation files (the "Software"), to deal
+    -- in the Software without restriction, including without limitation the rights
+    -- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    -- copies of the Software, and to permit persons to whom the Software is
+    -- furnished to do so, subject to the following conditions:
+    --
+    -- The above copyright notice and this permission notice shall be included in
+    -- all copies or substantial portions of the Software.
+    --
+    -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    -- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    -- THE SOFTWARE.
+    -------------------------------------------------------------------------------
+
+    Project     : FFT_CORE
+    Author      : Konovalov Vitaliy 
+    Description : 
+   
+*/
+package axis_pkg;
+
+	typedef struct {
+		logic [31:0] re;
+		logic [31:0] im;
+	} sample_t;
+
+	sample_t sample0 = '{0,0};
+
+	typedef struct {
+		sample_t tdata;
+		logic    tlast;
+	} axis_t;
+
+	axis_t axis_trans0 = '{sample0,0};
+
+endpackage
