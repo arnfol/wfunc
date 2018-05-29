bef840f48c601beb683d73f99fbe3c036d6783b8
arnfol <arnfol@mail.ru>
2018-05-29 22:04:01 +0300

    src. compile define-tasks & some fixes

--- common/axis_defines.svh
+++ common/axis_defines.svh
@@ -1,94 +1,112 @@
 
 
 
-// // ------------------------------------------------------------------------------
-// // --  TASKS FOR TESTING
-// // ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+// --  TASKS FOR TESTING
+// ------------------------------------------------------------------------------
 
-// // ------------------------------------------
-// // send data tasks
+`define AXIS_MST(pref)\
+\
+logic  ``pref``_tclk  ;\
+logic  ``pref``_tvalid;\
+logic  ``pref``_tready;\
+axis_t ``pref``_tr    ;\
+axis_t ``pref``_history[$];\
+\
+\
+/* low-level tasks*/\
+task automatic ``pref``_cyc_wait(int cycles = 1);\
+	repeat(cycles) @(posedge ``pref``_tclk);\
+endtask\
+\
+function automatic ``pref``_hshake();\
+	``pref``_hshake = ``pref``_tvalid && ``pref``_tready;\
+endfunction\
+\
+function automatic ``pref``_eop();\
+	``pref``_eop = ``pref``_hshake && ``pref``_tr.tlast;\
+endfunction \
+\
+/* monitor tasks */\
+task automatic ``pref``_mon(string src="AXIS_SLV", msg="get", bit verbose=1, bit save=0);\
+	do @(posedge ``pref``_tclk); while(!``pref``_hshake());\
+	if(verbose) $display("%t : %9s : %s",$time, src, msg);\
+	if(save) ``pref``_history.push_front(``pref``_tr);\
+endtask\
+\
+task automatic ``pref``_mon_pack(string src="AXIS_SLV", msg="get", bit verbose=1, bit save=0);\
+	do ``pref``_mon(src,msg,verbose,save); while(!``pref``_eop());\
+endtask\
+\
+/* transmit tasks */\
+task automatic ``pref``_send(axis_t data);\
+	``pref``_tr     <= data;\
+	``pref``_tvalid <= 1;\
+	do @(posedge ``pref``_tclk); while(!``pref``_hshake());\
+	``pref``_tr     <= axis_trans0;\
+	``pref``_tvalid <= 0;\
+endtask\
+\
+/* data       -- data words for packet */\
+/* wait_seed  -- max number of wait states between transactions (wait states number is a random value) */\
+task automatic ``pref``_pack(sample_t data[], int wait_seed=0);\
+	axis_t tmp;\
+	foreach(data[i]) begin\
+		tmp.tdata = data[i]; \
+		tmp.tlast = (i==0); \
+		if(wait_seed != 0) ``pref``_cyc_wait($urandom_range(wait_seed));\
+		``pref``_send(tmp);\
+	end\
+endtask
 
-// // data       -- data words for packet
-// // wait_seed  -- max number of wait states between transactions (wait states number is a random value)
-// task automatic pack(sample_t data[], int wait_seed=0);
-// 	axis_t tmp;
-
-// 	foreach(data[i]) begin
-// 		tmp.tdata = data[i]; 
-// 		tmp.tlast = (i==0); 
-
-// 		if(wait_seed != 0) cyc_wait($urandom_range(wait_seed));
-// 		send(tmp);
-// 	end
-
-// endtask
-
-// // simple axis transaction
-// task automatic send(axis_t data);
-// 	trans  <= data;
-// 	tvalid <= 1;
-
-// 	do @(posedge tclk); while(!hshake());
-
-// 	trans  <= axis_trans0;
-// 	tvalid <= 0;
-
-// endtask
-
-// // ------------------------------------------
-// // receive & monitor data tasks
-
-// // receive axis transaction
-// task automatic get(int wait_for=0, bit rand_wait=0);
-// 	if(wait_for != 0) begin
-// 		if(rand_wait) cyc_wait($urandom_range(wait_for));
-// 		else          cyc_wait(wait_for);
-// 	end
-
-// 	tready <= 1;
-// 	do @(posedge tclk); while(!hshake());
-// 	tready <= 0;
-// endtask
-
-// // receive axis packet
-// task automatic get_pack(int wait_for=0, bit rand_wait=1);
-// 	do begin
-// 		get(wait_for,rand_wait);
-// 	end while (!eop());
-// endtask
-
-// // simple axis monitor with history save
-// task automatic mon(string src="AXIS_SLV", msg="get", bit verbose=1, bit save=0);
-// 	do @(posedge tclk); while(!hshake());
-
-// 	if(verbose) $display(
-// 		"%t : %9s : %s data.Re=%8h, data.im=%8h, tlast=%b",
-// 		$time, src, msg, trans.tdata.re, trans.tdata.im, trans.tlast);
-
-// 	if(save) history.push_front(trans);
-// endtask
-
-// // monitor full packet
-// task automatic mon_pack(string src="AXIS_SLV", msg="get", bit verbose=1, bit save=0);
-// 	do mon(src,msg,verbose,save); while(!eop());
-// endtask
 
       
-`define LOW_LVL_AXIS_TASKS(pref)                           \
-                                                           \
-logic  ``pref``_tclk  ;                                    \
-logic  ``pref``_tvalid;                                    \
-logic  ``pref``_tready;                                    \
-axis_t ``pref``_tr    ;                                    \
-                                                           \
-task automatic ``pref``_cyc_wait(int cycles = 1);          \
-	repeat(cycles) @(posedge ``pref``_tclk);               \
-endtask                                                    \
-                                                           \
-function automatic ``pref``_hshake();                      \
-	``pref``_hshake = ``pref``_tvalid && ``pref``_tready;  \
-endfunction                                                \
-                                                           \
-function automatic ``pref``_eop();                         \
-	``pref``_eop = ``pref``_hshake && ``pref``_tr.tlast;   \
-endfunction 
+`define AXIS_SLV(pref)\
+\
+logic  ``pref``_tclk  ;\
+logic  ``pref``_tvalid;\
+logic  ``pref``_tready;\
+axis_t ``pref``_tr    ;\
+axis_t ``pref``_history[$];\
+\
+\
+/* low-level tasks*/\
+task automatic ``pref``_cyc_wait(int cycles = 1);\
+	repeat(cycles) @(posedge ``pref``_tclk);\
+endtask\
+\
+function automatic ``pref``_hshake();\
+	``pref``_hshake = ``pref``_tvalid && ``pref``_tready;\
+endfunction\
+\
+function automatic ``pref``_eop();\
+	``pref``_eop = ``pref``_hshake && ``pref``_tr.tlast;\
+endfunction \
+\
+/* monitor tasks */\
+task automatic ``pref``_mon(string src="AXIS_SLV", msg="get", bit verbose=1, bit save=0);\
+	do @(posedge ``pref``_tclk); while(!``pref``_hshake());\
+	if(verbose) $display("%t : %9s : %s",$time, src, msg);\
+	if(save) ``pref``_history.push_front(``pref``_tr);\
+endtask\
+\
+task automatic ``pref``_mon_pack(string src="AXIS_SLV", msg="get", bit verbose=1, bit save=0);\
+	do ``pref``_mon(src,msg,verbose,save); while(!``pref``_eop());\
+endtask\
+\
+/* receive tasks */\
+task automatic ``pref``_get(int wait_for=0, bit rand_wait=0);\
+	if(wait_for != 0) begin\
+		if(rand_wait) ``pref``_cyc_wait($urandom_range(wait_for));\
+		else          ``pref``_cyc_wait(wait_for);\
+	end\
+	``pref``_tready <= 1;\
+	do @(posedge ``pref``_tclk); while(!``pref``_hshake());\
+	``pref``_tready <= 0;\
+endtask\
+\
+task automatic ``pref``_get_pack(int wait_for=0, bit rand_wait=1);\
+	do begin\
+		``pref``_get(wait_for,rand_wait);\
+	end while (!``pref``_eop());\
+endtask
