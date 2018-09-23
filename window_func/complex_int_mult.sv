7e6b2c92b508bb055ab931d6e3b1cbd8f37856f4
arnfol <arnfol@mail.ru>
2018-09-23 14:26:38 +0300

    manually pipeline comlex multiplier logic

--- window_func/complex_int_mult.sv
+++ window_func/complex_int_mult.sv
@@ -26,35 +26,67 @@
     Project     : FFT_CORE
     Author      : Konovalov Vitaliy
     Description :
-                  Very simplified model of complex multimplier in int.
+                  Pipelined complex multimplier in int.
 
 */
 module complex_int_mult
     import axis_pkg::*;
 #(
-    PIPE_NUM = 10,
+    PIPE_NUM = 10, // should be > 2
     DISPLNUM = 0
 ) (
     input               clk  ,
     input               rst_n,
-    input               en   ,
+    input               en   , // enable of the module, 0 stops operation
     input  sample_t_int a    , // input 1
     input  sample_t_int b    , // input 2
     output sample_t     z      // result
 );
 
-    sample_t z_reg[PIPE_NUM]; // not pipelined result
+    localparam ADD_PIPE_NUM = PIPE_NUM-2;
 
-    // pipeline
+    sample_t z_reg[ADD_PIPE_NUM]; 
+    sample_t_int a_del, b_del;
+    logic signed [31:0] are_bre, aim_bim, are_bim, aim_bre;
+
+
+
+    // input registers
+    always_ff @(posedge clk or negedge rst_n) begin : proc_ab_delay
+        if(~rst_n) begin
+            a_del <= '{0,0}; 
+            b_del <= '{0,0};
+        end else if(en) begin
+            a_del <= a; 
+            b_del <= b;
+        end
+    end
+
+    // multiplication registers
+    always_ff @(posedge clk or negedge rst_n) begin : proc_mult
+        if(~rst_n) begin
+            are_bre <= '0;
+            aim_bim <= '0;
+            are_bim <= '0;
+            aim_bre <= '0;
+        end else if(en) begin
+            are_bre <= a_del.re*b_del.re; 
+            aim_bim <= a_del.im*b_del.im;
+            are_bim <= a_del.re*b_del.im; 
+            aim_bre <= a_del.im*b_del.re;
+        end
+    end
+
+    // sum register & additional pipeline for retiming
     always_ff @(posedge clk or negedge rst_n) begin : proc_z_reg
         if(~rst_n) begin
-            for (int i = 0; i < PIPE_NUM; i++) begin
+            for (int i = 0; i < ADD_PIPE_NUM; i++) begin
                 z_reg[i] <= '{0,0};
             end 
         end else if(en) begin
-            z_reg[0].re <= a.re*b.re - a.im*b.im;
-            z_reg[0].im <= a.re*b.im + a.im*b.re;
-            for (int i = 1; i < PIPE_NUM; i++) begin
+            z_reg[0].re <= are_bre - aim_bim;
+            z_reg[0].im <= are_bim + aim_bre;
+            for (int i = 1; i < ADD_PIPE_NUM; i++) begin
                 z_reg[i] <= z_reg[i-1];
             end 
         end
@@ -71,6 +103,6 @@ module complex_int_mult
     // translate_on
 
 
-    assign z = z_reg[PIPE_NUM-1];
+    assign z = z_reg[ADD_PIPE_NUM-1];
 
 endmodule
